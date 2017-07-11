427c94b37eeb0c8705fbf7e6377edced9b361c38
rossialice <alicerossi00@libero.it>
2017-07-11 11:43:29 +0200

    ordering buttons

--- src/gui.py
+++ src/gui.py
@@ -15,19 +15,27 @@ class GUI(tk.Frame):
         self.create_widgets()
 
     def play(self):
-        print("Play!")    
+        print("Play!")
+        self.graph_button.configure(state="active")
 
     def stop(self):
-        print("Stop!")    
-	
+        print("Stop!")
+
+
     def next(self):
         print("Next!")   
 
     def map(self):
-        print("Map!")   
+        print("Map!")
+        self.stop_button.configure(state="active")
+        self.next_button.configure(state="active")
+        self.play_button.configure(state="active")
+        self.map_button.configure(state="disabled")
 	
     def log(self):
-        print("Log!") 
+        print("Log!")
+        self.log_button.configure(state="disabled")
+        self.map_button.configure(state="active")
 	
     def graph(self):
         print("Graph!")
@@ -43,6 +51,7 @@ class GUI(tk.Frame):
         play_icon = ImageTk.PhotoImage(play_image)
         self.play_button = Button(self.master, text="Play", command=self.play, image=play_icon)
         self.play_button.image = play_icon  # keep a reference!
+        self.play_button.configure(state="disabled")
         self.play_button.pack(side="left")
 
 		  
@@ -50,18 +59,23 @@ class GUI(tk.Frame):
         stop_icon = ImageTk.PhotoImage(stop_image)
         self.stop_button = Button(self.master, text="Stop", command=self.stop, image=stop_icon)
         self.stop_button.image = stop_icon  # keep a reference!
+        self.stop_button.configure(state="disabled")
         self.stop_button.pack(side="left")
-		
+
+
+
         next_image = Image.open('icon/next.png')
         next_icon = ImageTk.PhotoImage(next_image)
         self.next_button = Button(self.master, text="next", command=self.next, image=next_icon)
         self.next_button.image = next_icon  # keep a reference!
+        self.next_button.configure(state="disabled")
         self.next_button.pack(side="left")
 		
         map_image = Image.open('icon/map.png')
         map_icon = ImageTk.PhotoImage(map_image)
         self.map_button = Button(self.master, text="map", command=self.map, image=map_icon)
         self.map_button.image = map_icon  # keep a reference!
+        self.map_button.configure(state="disabled")
         self.map_button.pack(side="left")
 		
         log_image = Image.open('icon/log.png')
@@ -74,6 +88,7 @@ class GUI(tk.Frame):
         graph_icon = ImageTk.PhotoImage(graph_image)
         self.graph_button = Button(self.master, text="graph", command=self.graph, image=graph_icon)
         self.graph_button.image = graph_icon  # keep a reference!
+        self.graph_button.configure(state="disabled")
         self.graph_button.pack(side="left")
 		
         about_image = Image.open('icon/about.png')
